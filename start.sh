53b7e9f4f06cb1917862629d4b412492c4180548
NIK0605 <109219388+Nikhilagrawalcoder@users.noreply.github.com>
2025-02-07 15:32:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Install Chromium
+# Update system and install necessary dependencies for Chromium
 apt-get update
 apt-get install -y chromium
+apt-get install -y chromium-driver
 
-# Set the Chrome binary path
+# Set environment variables for Chrome and Chromedriver
 export CHROME_BIN=/usr/bin/chromium
+export CHROME_DRIVER=/usr/lib/chromium-browser/chromedriver
 
-# Start the Flask app with Gunicorn
+# Start the Flask application using Gunicorn
 gunicorn -b 0.0.0.0:10000 app:app
