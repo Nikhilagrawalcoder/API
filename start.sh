6c57a78144fe6fc103c4c3b2545c264f45408faf
NIK0605 <109219388+Nikhilagrawalcoder@users.noreply.github.com>
2025-02-07 15:28:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Install Chromium
+apt-get update
+apt-get install -y chromium
+
+# Set the Chrome binary path
+export CHROME_BIN=/usr/bin/chromium
+
+# Start the Flask app with Gunicorn
 gunicorn -b 0.0.0.0:10000 app:app
